dce198dcb0d260db83fcd28c8186b1934798cfdc
lioussajr3000 <142400477+lioussajr3000@users.noreply.github.com>
2023-08-30 16:13:15 +0000

    Create pannier.js

---
+++ pannier.js
@@ -0,0 +1,9 @@
+
+function ggg () {
+    var btn= pannier.getElementById(plus)
+    btn.addEventListener('click', foo);
+    onclick='confirmernombre'(console.log("un click"))
+    ClipboardEvent.btn=0 
+    if (ClipboardEvent.btn=1)
+    console.log("un click")
+}
